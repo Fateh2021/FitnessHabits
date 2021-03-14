3fab1d6a72ebeb22c4c4347d53746849687b942a
david <adjelek.dav@gmail.com>
2021-03-14 13:01:07 -0400

    Ajout des configuration pour l'authentification google; Ajout de formatage css supplémentaire pour la page de connexion et Ajout des boutons connexion facebook et google

--- src/pages/Login/LogIn.jsx
+++ src/pages/Login/LogIn.jsx
@@ -1,8 +1,11 @@
 import * as firebase from 'firebase'
 import "firebase/auth";
 import React, { useState } from 'react';
-import { IonInput, IonGrid, IonItem, IonRow, IonCol, IonPage, IonButton, IonLoading } from '@ionic/react';
+import { IonInput, IonGrid, IonItem, IonRow, IonCol, IonPage, IonButton, IonLoading, IonIcon } from '@ionic/react';
 import { toast } from '../../Toast'
+/* les imports dont GEFRAL a besoin  */
+import { signInWithGoogle } from '../../firebaseConfig';
+import {logoFacebook,logoGoogle,arrowForward} from "ionicons/icons";
 
 const LogIn = (props) => {    
     const [username, setUsername]= useState('')
@@ -24,17 +27,42 @@ const LogIn = (props) => {
     }
 
     return (
-      <IonPage className="fondIntro">
-        <IonGrid >
+      <IonPage>
+          <img id="img" src="/assets/backgroundColor.png" alt=""/>
+        <IonGrid>
           <IonRow className="">
             <IonCol>
             </IonCol>
             <IonCol size="8">
-              <img className="logoIntro" src="/assets/Logo2.png" alt="" />  
+              <img className="logoIntro" src="/assets/LogoLegion.png" alt="" />
             </IonCol>
             <IonCol>
             </IonCol>
           </IonRow>
+            <IonRow class="row-facebook-google-format">
+                <IonCol>
+                    <IonIcon class="google-facebook-logo-left" size="large" icon={logoGoogle}/>
+                </IonCol>
+                <IonCol >
+                    <IonButton color="dark" class="google-facebook-button-right" onClick={login} >
+                        Se connecter via google
+                        <IonIcon icon={arrowForward}/>
+                    </IonButton>
+                </IonCol>
+            </IonRow>
+            <IonRow class="rowAlign">
+                <IonCol>
+                    <IonIcon class="google-facebook-logo-left" size="large" icon={logoFacebook}/>
+                </IonCol>
+                <IonCol>
+                    <IonButton color="dark" class="google-facebook-button-right" onClick={login}>
+                        Se connecter via Facebook
+                        <IonIcon icon={arrowForward}/>
+                    </IonButton>
+                </IonCol>
+            </IonRow>
+
+            <IonRow class="rowAlignOr">OU</IonRow>
         <IonRow className="">
           <IonCol>
             <IonLoading message="Veuillez patienter.." duration={0} isOpen={busy} />   
