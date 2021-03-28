534d5b31854436d09aa3722dae5a41043b47fc75
lacan14 <alexandre.tran27@hotmail.com>
2021-03-27 20:26:05 -0400

    persistance du choix de la langue

--- src/translate/Translator.js
+++ src/translate/Translator.js
@@ -1,22 +1,40 @@
+import * as firebase from 'firebase'
+const dict = require('./Translation.json');
+const supportedLanguages = ["en", "es", "fr"];
 
-var preferredLanguage = "en";
-var dict = require('./Translation.json');
-
-
-exports.setLang = function(lang) {
-    if (["en","es","fr"].includes(lang)) {
-        preferredLanguage = lang;
+export function setLang(lang) {
+    const userUID = localStorage.getItem('userUid');
+    if (supportedLanguages.includes(lang)) {
+        localStorage.setItem("userLanguage", lang);
+        var field = { "langue": lang }
+        firebase.database().ref('settings/' + userUID).update(field);
     }
 }
 
-exports.getLang = function() {
-    return preferredLanguage;
+export function getLang() {
+    var language = "";
+    
+    if (localStorage["userLanguage"]) {
+        language = localStorage.getItem("userLanguage");
+    } else {
+        const userUID = localStorage.getItem('userUid');
+        firebase.database().ref('settings/' + userUID).once('value', (snapshot) => {
+            const data = snapshot.val();
+            language = data["langue"];
+        });        
+    }
+
+    if (language == "") {
+        var lang = window.navigator.userLanguage
+            || window.navigator.language;
+        lang = lang.substring(0, 2);
+        if (supportedLanguages.includes(lang)) {
+            language = lang;
+        }
+    }
+    return language;
 }
 
-exports.getText = (key) => {
-    return dict[key][preferredLanguage];
+export function getText(key) {
+    return dict[key][getLang()];
 }
-
-exports.preferredLanguage = preferredLanguage;
-exports.dict = dict;
-
