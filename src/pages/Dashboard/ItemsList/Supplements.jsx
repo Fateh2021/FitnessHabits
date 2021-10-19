4fa19f5884c5efa109222e910b3026e74f14cb6a
Yessenia Munoz <munoz_monroy.neida_yessenia@courrier.uqam.ca>
2021-10-18 23:35:05 -0400

    Ajout menu suppléments et médicaments

--- src/pages/Dashboard/ItemsList/Supplements.jsx
+++ src/pages/Dashboard/ItemsList/Supplements.jsx
@@ -1,11 +1,20 @@
-import React from "react"
-import { IonInput, IonRow, IonIcon, IonLabel, IonItem, IonAvatar, IonCol} from '@ionic/react';
+import React, {useEffect, useState} from "react"
+import firebase from 'firebase'
+import { IonInput, IonList, IonRow, IonIcon, IonLabel, IonItem, IonAvatar, IonCol} from '@ionic/react';
 import {arrowDropdownCircle} from 'ionicons/icons';
 
 import '../../Tab1.css';
 
-const Supplements = () => {
+const Supplements = (props) => {
 
+  const accor = (divId) => {
+    const divElt=document.getElementById(divId);
+    if (divElt) {
+      (!divElt.style.display || divElt.style.display === "none") ? divElt.style.display = "block":divElt.style.display = "none";
+    }    
+  }
+
+  
   return (
     <div>
       <IonItem className="divTitre3">
@@ -16,19 +25,56 @@ const Supplements = () => {
           <h2><b>Suppléments</b></h2>
         </IonLabel>
         <IonInput className='inputTextGly' readonly color="danger" value={""}></IonInput>
-        <IonIcon className="arrowDashItem" icon={arrowDropdownCircle} onClick={""}/>
+        <IonIcon className="arrowDashItem" icon={arrowDropdownCircle} onClick={() => accor("myDIVSuppl")}/>
       </IonItem>
-      <div id="myDIV3">
-        <IonItem>     
-          <IonRow>
-            <IonCol size="6">
-              <IonLabel className = 'joggingTitle'><h3>B12</h3></IonLabel>
-            </IonCol>
-            <IonCol size="6" >
-              <IonInput className='inputTextActivities' color="danger" value={""} placeholder="______" onIonChange={""}></IonInput>  
-            </IonCol>
-          </IonRow>
-        </IonItem>
+      <div id="myDIVSuppl">
+        <IonList>
+          <IonItem className="trashButton" color="danger">
+            <IonAvatar slot="start">
+              <img src="/assets/suppl/resumen.png" alt=""/>
+            </IonAvatar>
+            <IonLabel>
+              <h2><b>Liste Suppléments et médicaments</b></h2>
+            </IonLabel>
+            <IonIcon className="arrowDashItem" icon={arrowDropdownCircle} onClick={""}/>
+          </IonItem>
+          <IonItem className="trashButton" color="danger">
+            <IonAvatar slot="start">
+              <img src="/assets/suppl/activer.png" alt=""/>
+            </IonAvatar>
+            <IonLabel>
+              <h2><b>Activer ou Désactiver Suppléments et médicaments</b></h2>
+            </IonLabel>
+            <IonIcon className="arrowDashItem" icon={arrowDropdownCircle} onClick={""}/>
+          </IonItem>
+          <IonItem className="trashButton" color="danger">
+            <IonAvatar slot="start">
+              <img src="/assets/suppl/plus.png" alt=""/>
+            </IonAvatar>
+            <IonLabel>
+              <h2><b>Saisir supplément ou médicament</b></h2>
+            </IonLabel>
+            <IonIcon className="arrowDashItem" icon={arrowDropdownCircle} onClick={""}/>
+          </IonItem>
+          <IonItem className="trashButton" color="danger">
+            <IonAvatar slot="start">
+              <img src="/assets/suppl/respect.png" alt=""/>
+            </IonAvatar>
+            <IonLabel>
+              <h2><b>Respect de la posologie</b></h2>
+            </IonLabel>
+            <IonIcon className="arrowDashItem" icon={arrowDropdownCircle} onClick={""}/>
+          </IonItem>
+          <IonItem className="trashButton" color="danger">
+            <IonAvatar slot="start">
+              <img src="/assets/suppl/ecart.png" alt=""/>
+            </IonAvatar>
+            <IonLabel>
+              <h2><b>Afficher les écarts</b></h2>
+            </IonLabel>
+            <IonIcon className="arrowDashItem" icon={arrowDropdownCircle} onClick={""}/>
+          </IonItem>
+        </IonList>
       </div>
     </div>
   );
