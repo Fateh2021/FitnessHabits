3daf3c9d885f6ab33f210da2d7e43b2213d8b7ee
Mohamed Fouad Serradj <mohamedfouad.serradj@concordia.ca>
2021-11-02 16:47:13 -0400

    Creation de la fonction d'ajout de supplements & medicaments

--- src/pages/Dashboard/ItemsList/Supplements.jsx
+++ src/pages/Dashboard/ItemsList/Supplements.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react"
 import firebase from 'firebase'
-import { IonInput, IonList, IonRow, IonIcon, IonLabel, IonItem, IonAvatar, IonCol} from '@ionic/react';
+import { IonInput, IonList, IonButton, IonIcon, IonLabel, IonItem, IonAvatar, IonSelectOption, IonSelect, IonRadioGroup, IonListHeader, IonDatetime, IonToggle, IonCheckbox} from '@ionic/react';
 import {arrowDropdownCircle} from 'ionicons/icons';
 
 import '../../Tab1.css';
@@ -46,26 +46,124 @@ const Supplements = (props) => {
             <IonLabel>
               <h2><b>Ajouter suppléments/médicaments</b></h2>
             </IonLabel>
-            <IonIcon className="arrowDashItem" icon={arrowDropdownCircle} onClick={""}/>
+            <IonIcon className="arrowDashItem" icon={arrowDropdownCircle} onClick={() => accor("myDIVAjoutSupp1")}/>
           </IonItem>
-          <IonItem className="trashButton" color="danger">
-            <IonAvatar slot="start">
-              <img src="/assets/suppl/resumen.png" alt=""/>
-            </IonAvatar> 
-            <IonLabel>
-              <h2><b>Lister suppléments/médicaments</b></h2>
-            </IonLabel>
-            <IonIcon className="arrowDashItem" icon={arrowDropdownCircle} onClick={""}/>
-          </IonItem>
-          <IonItem className="trashButton" color="danger">
-            <IonAvatar slot="start">
-              <img src="/assets/suppl/ecart.png" alt=""/>
-            </IonAvatar>
-            <IonLabel>
-              <h2><b>Afficher les écarts</b></h2>
-            </IonLabel>
-            <IonIcon className="arrowDashItem" icon={arrowDropdownCircle} onClick={""}/>
-          </IonItem>
+          <div id = "myDIVAjoutSupp1">
+              <IonList>
+                <IonItem>
+                  <IonLabel color="light">Nom</IonLabel>
+                  <IonInput className="inputSuppConsom"></IonInput>
+                </IonItem>
+                <IonItem>
+                  <IonLabel color="light">Dose</IonLabel>
+                  <IonInput className="inputSuppConsom"></IonInput>
+                  <IonSelect value={""} placeholder="Unité" className="inputSuppConsom">
+                    <IonSelectOption value="oz">Oz</IonSelectOption>
+                    <IonSelectOption value="ml">Ml</IonSelectOption>
+                    <IonSelectOption value="unite">Unité</IonSelectOption>
+                  </IonSelect>
+                </IonItem>
+                <IonList>
+                  <IonRadioGroup>
+                    <IonItem>
+                      <IonLabel color="light" >Supplément</IonLabel>
+                      <ion-radio slot="start"  value="supplement"></ion-radio>
+                    </IonItem>
+                    <IonItem>
+                      <IonLabel color="light" >Medicament</IonLabel>
+                      <ion-radio slot="start" value="medicament"></ion-radio>
+                    </IonItem>
+                  </IonRadioGroup>
+                </IonList>
+                <IonItem>
+                  <IonLabel color="light" >Date de debut</IonLabel>
+                  <ion-datetime value="2021-10-01T15:43:40.394Z" display-timezone="utc"></ion-datetime>
+                </IonItem>
+                <IonItem>
+                  <IonLabel color="light" >Date de fin</IonLabel>
+                  <IonDatetime value="2021-12-10T15:43:40.394Z" display-timezone="utc"></IonDatetime>
+                </IonItem>
+                <IonItem>
+                  <IonLabel slot = "start" color="light">Actif</IonLabel>
+                  <IonToggle name="actif" color="success" checked ></IonToggle>
+                </IonItem>
+                <IonItem>
+                  <IonLabel color="light">Posologie</IonLabel>
+                  <IonInput className="inputSuppConsom" ></IonInput>
+                  <IonLabel color="light" >Dose par</IonLabel>
+                  <IonInput className="inputSuppConsom" ></IonInput>
+                  <IonSelect value={""} placeholder="Unité" className="inputSuppConsom">
+                    <IonSelectOption value="oz">Oz</IonSelectOption>
+                    <IonSelectOption value="ml">Ml</IonSelectOption>
+                    <IonSelectOption value="unite">Unité</IonSelectOption>
+                  </IonSelect>
+                </IonItem>
+                <IonItem>
+                  <IonLabel color="light" >Lundi</IonLabel>
+                  <IonCheckbox color="primary" checked slot="start" ></IonCheckbox>
+                  <IonLabel color="light" slot="start">Mardi</IonLabel>
+                  <IonCheckbox color="primary" checked ></IonCheckbox>
+                </IonItem>
+                <IonItem>
+                  <IonLabel color="light">Mercredi</IonLabel>
+                  <IonCheckbox color="primary" checked slot="start" ></IonCheckbox>
+                  <IonLabel color="light" slot="start">Jeudi</IonLabel>
+                  <IonCheckbox color="primary" checked ></IonCheckbox>
+                </IonItem>
+                <IonItem>
+                  <IonLabel color="light" >Vendredi</IonLabel>
+                  <IonCheckbox color="primary" checked slot="start" ></IonCheckbox>
+                  <IonLabel color="light" slot="start">Samedi</IonLabel>
+                  <IonCheckbox color="primary" checked ></IonCheckbox>
+                </IonItem>
+                <IonItem>
+                  <IonLabel color="light" >Dimanche</IonLabel>
+                  <IonCheckbox color="primary" checked slot="start" ></IonCheckbox>
+                </IonItem>
+                <IonListHeader>
+                  <IonLabel color="light" >Heures</IonLabel>
+                </IonListHeader>
+                <IonList>
+                <IonItem>
+                  <IonLabel color="light" >Matin</IonLabel>
+                  <IonDatetime display-format="h:mm A" picker-format="h:mm A" value="1990-02-19T11:00Z"></IonDatetime>
+                  <IonDatetime display-format="h:mm A" picker-format="h:mm A" value="1990-02-19T14:00Z"></IonDatetime>
+                </IonItem>
+                <IonItem>
+                  <IonLabel color="light" >Midi</IonLabel>
+                  <IonDatetime display-format="h:mm A" picker-format="h:mm A" value="1990-02-19T17:00Z"></IonDatetime>
+                  <IonDatetime display-format="h:mm A" picker-format="h:mm A" value="1990-02-19T19:00Z"></IonDatetime>
+                </IonItem>
+                <IonItem>
+                  <IonLabel color="light" >Soir</IonLabel>
+                  <IonDatetime display-format="h:mm A" picker-format="h:mm A" value="1990-02-19T00:00Z"></IonDatetime>
+                  <IonDatetime display-format="h:mm A" picker-format="h:mm A" value="1990-02-19T03:00Z"></IonDatetime>
+                </IonItem>
+                </IonList>
+                <IonItem>
+                  <IonButton  color="success">Enregistrer</IonButton >
+                  <IonButton color="danger" >Annuler</IonButton >
+                </IonItem>
+              </IonList>
+            </div>
+            <IonItem className="trashButton" color="danger">
+              <IonAvatar slot="start">
+                <img src="/assets/suppl/resumen.png" alt=""/>
+              </IonAvatar> 
+              <IonLabel>
+                <h2><b>Lister suppléments/médicaments</b></h2>
+              </IonLabel>
+              <IonIcon className="arrowDashItem" icon={arrowDropdownCircle} onClick={""}/>
+            </IonItem>
+            <IonItem className="trashButton" color="danger">
+              <IonAvatar slot="start">
+                <img src="/assets/suppl/ecart.png" alt=""/>
+              </IonAvatar>
+              <IonLabel>
+                <h2><b>Afficher les écarts</b></h2>
+              </IonLabel>
+              <IonIcon className="arrowDashItem" icon={arrowDropdownCircle} onClick={""}/>
+            </IonItem>
         </IonList>
       </div>
     </div>
