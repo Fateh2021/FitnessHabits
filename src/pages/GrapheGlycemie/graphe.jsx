327cc957eae24f03dc889ae75e87dca32d5aa124
Bigras, Julien <bigras.julien@courrier.uqam.ca>
2021-11-12 23:20:58 -0500

    Correction pour nouveaux boutons

--- src/pages/GrapheGlycemie/graphe.jsx
+++ src/pages/GrapheGlycemie/graphe.jsx
@@ -3,18 +3,22 @@ import '../Tab1.css'
 import moment from "moment";
 import firebase from 'firebase'
 import "firebase/auth";
+import * as translate from '../../translate/Translator';
 
 import CanvasJSReact from './canvasjs.stock.react';
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 const Graphe = (reloadGraph) => {
     const today = new Date(2021, 9, 18);// Using preset date for testing
-
     const [data, setData] = useState([]);
     const [tauxLabel, setTauxLabel] = useState(" mmol/L"); //" mmol/L" | " mg/dl"
     const [endDate, setEndDate] = useState(today);
     const [startDate, setStartDate] = useState(() => getNewStartDateForRange('3m'));
-
+    const averageText = translate.getText("AVERAGE");
+    const sugarLevel = translate.getText("BLOOD_SUGAR_LEVEL");
+    const weekLetter = translate.getText("WEEK_LETTER");
+    const yearLetter = translate.getText("YEAR_LETTER");
+    
     useEffect(() => {
         if (reloadGraph && dataPoints.length <= 0)
             fetchDatapoints();
@@ -36,29 +40,32 @@ const Graphe = (reloadGraph) => {
                 arr.sort((a, b) => {
                     return a.x - b.x;
                 });
-
+                
                 setData(arr)
+
             });
     }
 
     function toMmoll(dp) {
         //  mgdl / 18
-        
-        for (var i = 0; i < dp.length;i++) {
-            dp[i].y = dp[i].y / 18;
+        var temp = dp.slice();
+
+        for (var i = 0; i < temp.length;i++) {
+            temp[i].y = temp[i].y / 18;
         }
 
-        return dp;
+        return temp;
     }
 
     function toMgdl(dp) {
         // mmol * 18
+        var temp = dp.slice();
 
-        for (var i = 0; i < dp.length;i++) {
-            dp[i].y = dp[i].y * 18;
+        for (var i = 0; i < temp.length;i++) {
+            temp[i].y = temp[i].y * 18;
         }
 
-        return dp;
+        return temp;
     }
 
     const dataPoints = useMemo(
@@ -66,11 +73,13 @@ const Graphe = (reloadGraph) => {
         [data, startDate, endDate]
     );
 
-
     const [average, setAverage] = useState(0);
     const [averageDataPoints, setAverageDataPoint] = useState([]);
     useEffect(() => {
         if (dataPoints && dataPoints.length > 0) {
+
+            console.log(dataPoints)
+
             let av = Math.round((((dataPoints.map(val => val.y)).reduce((a, b) => (a + b), 0) / dataPoints.length) + Number.EPSILON) * 100) / 100;
             
             setAverage(av)
@@ -79,6 +88,7 @@ const Graphe = (reloadGraph) => {
                 { x: endDate, y: av }
             ])
             console.log(averageDataPoints)
+            
         }
     }, [dataPoints]);
 
@@ -112,6 +122,17 @@ const Graphe = (reloadGraph) => {
         setEndDate(moment(event.target.value).toDate());
     }
 
+    function onUnitTypeChange(unit) {
+        if(unit !== tauxLabel) {
+            setTauxLabel(unit);
+            if(unit === " mg/dl") {
+                setData(toMgdl(data))
+            } else {
+                setData(toMmoll(data))
+            }
+        }
+    }
+
 
 
     if (!dataPoints || dataPoints.length <= 0 || !averageDataPoints || averageDataPoints.length <= 0) {
@@ -131,7 +152,7 @@ const Graphe = (reloadGraph) => {
                 valueFormatString: "DD-MM-YYYY"
             },
             axisY: {
-                title: "Taux glycemie",
+                title: sugarLevel,
                 suffix: tauxLabel
             },
             data: [
@@ -148,14 +169,19 @@ const Graphe = (reloadGraph) => {
             ]
         };
         return (
+            
             <div style={{ overflowX: "scroll" }}>
-                <h3 style={{color: "#c0504e"}}>Moyenne: {average} {tauxLabel}</h3>
+                <h3 style={{color: "#c0504e"}}>{averageText}: {average} {tauxLabel}</h3>
                 <div>
-                    <button class="timeFilterFirst" onClick={() => { onRangeClick("1w"); }}><h3>1S</h3></button>
+                    <button class="timeFilterFirst" onClick={() => { onRangeClick("1w"); }}><h3>1{weekLetter}</h3></button>
                     <button class="timeFilter" onClick={() => { onRangeClick("1m"); }}><h3>1M</h3></button>
                     <button class="timeFilter" onClick={() => { onRangeClick("3m"); }}><h3>3M</h3></button>
                     <button class="timeFilter" onClick={() => { onRangeClick("6m"); }}><h3>6M</h3></button>
-                    <button class="timeFilter" onClick={() => { onRangeClick("1y"); }}><h3>1A</h3></button>
+                    <button class="timeFilter" onClick={() => { onRangeClick("1y"); }}><h3>1{yearLetter}</h3></button>
+                </div>
+                <div>
+                    <button class="timeFilterFirst" onClick={() => { onUnitTypeChange(" mg/dl"); }}><h3>mg/dl</h3></button>
+                    <button class="timeFilter" onClick={() => { onUnitTypeChange(" mmol/L"); }}><h3>mmol/L</h3></button> 
                 </div>
                 <div>
                     <h3>
