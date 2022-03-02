03ac6c0fa0df79ab6703d318f9600b62cf137432
David Genois <fc591912@ens.uqam.ca>
2022-03-02 16:55:37 -0500

    Nettoyage de TakePicture.jsx et optimisation des appels firebase

--- src/pages/TakePicture/TakePicture.jsx
+++ src/pages/TakePicture/TakePicture.jsx
@@ -2,19 +2,18 @@ import { IonImg, IonIcon, IonAvatar } from '@ionic/react';
 import React, { Component } from 'react';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { aperture } from 'ionicons/icons';
-import firebase from "firebase";// import ajouté par GEFRAL
-
-
+import firebase from "firebase";
 import { Plugins, CameraResultType } from '@capacitor/core';
 
-  const { Camera } = Plugins;
-  const INITIAL_STATE = {
-    photo: '',
-  };
+const { Camera } = Plugins;
+
+const INITIAL_STATE = {
+  photo: '',
+  needsLoading: true,
+};
 
 
 export class TakePicture extends Component {
-  
 
   constructor(props) {
     super(props);
@@ -24,77 +23,83 @@ export class TakePicture extends Component {
 
   async takePicture() {
     const image = await Camera.getPhoto({
-      quality: 90,
+      quality: 70,
       allowEditing: false,
-      resultType: CameraResultType.Uri
+      resultType: CameraResultType.Uri,
+      targetHeight: 200,
+      targetWidth: 200,
     });
-    var imageUrl = image.webPath;
-    this.setState({
-      photo: imageUrl
-    })
-    this.uploadToStorage();
 
+    this.setState({
+      photo: image.webPath,
+      needsLoading: false,
+    })
+
+    this.uploadToStorage();
   }
 
 
   uploadToStorage() {
-    // Conversion de l'image en blob -- AJOUT TEAM APY
-    const fileName = firebase.auth().currentUser.uid + "/profilPictures";
+    const fileName = "profilPictures/" + firebase.auth().currentUser.uid;
     var getFileBlob = function (url, cb) {
       var xhr = new XMLHttpRequest();
       xhr.open("GET", url);
       xhr.responseType = "blob";
-      xhr.addEventListener('load', function() {
+      xhr.addEventListener('load', function () {
         cb(xhr.response);
       });
       xhr.send();
     };
-    getFileBlob(this.state.photo, blob =>{
-        // Upload l'image avec comme path l'ID firebase de l'utilisateur avec comme filename profilPicture -- AJOUT TEAM APY
-        firebase.storage().ref(fileName).put(blob).then(function(snapshot) {
+    getFileBlob(this.state.photo, blob => {
+      firebase.storage().ref(fileName).put(blob).then(function (snapshot) {
         console.log('Upload réussi !');
         firebase.database().ref("profiles/" + firebase.auth().currentUser.uid).update({
-          "profilPicture" : fileName
+          "profilPicture": fileName
         });
-     });
-   })
-}
+      });
+    })
+  }
 
-// Downlad image from firebase storage and set photo state
-  downloadImage(){
-    console.log("downloadImage");
+  // Download image from firebase storage and set photo state
+  async downloadImage() {
+    // Si l'URL est connu.
+    const cachedURL = localStorage.getItem("profile-picture-cache");
+    if (cachedURL) {
+      this.setState({ photo: cachedURL, needsLoading: false });
+      return;
+    }
+
+    // Fetch image from storage or provider profile
     var storageRef = firebase.storage().ref();
-    var imageRef = storageRef.child(firebase.auth().currentUser.uid + "/profilPictures");
-    imageRef.getDownloadURL().then(function(url) {
-      console.log(url);
-      this.setState({
-        photo: url
-      })
-    }.bind(this));
+    var imageRef = storageRef.child("profilPictures/" + firebase.auth().currentUser.uid);
+    let photoUrl = '';
+
+    try {
+      photoUrl = await imageRef.getDownloadURL();
+    } catch (err) {
+      // Si aucune photo custom, on utilise la photo de Fb/Google
+      if (!photoUrl && firebase.auth().currentUser?.photoURL) {
+        photoUrl = firebase.auth().currentUser.photoURL;
+      }
+    }
+
+    localStorage.setItem("profile-picture-cache", photoUrl);
+    this.setState({ photo: photoUrl, needsLoading: false });
   }
 
 
-  
+
   render() {
-    
-    let { photo } = this.state;
-    /*GEFRAL: Dans le cas où l'utilisateur se connecte avec Google ou Facebook,
-    sa photo de profil de son compte utilisé pour la connexion est automatique ajouté à son profil
-    */
-    if (!photo ){ 
+    // TODO: add a test for method calls (upload/download)
+    let { photo, needsLoading } = this.state;
+    if (!photo && needsLoading) {
       this.downloadImage();
-      /*
-      const user = firebase.auth().currentUser;
-      if (user != null) {
-        photo = user.photoURL;//on récupère la photo de profil de son compte google
-      }
-      */
-    } 
-    
-    return (       
+    }
+
+    return (
       <div>
-        <button className="sideBarButtonCamera" onClick={() => this.takePicture()} color="danger">
-          <IonIcon icon={aperture}/>
+        <button className="sideBarButtonCamera" onClick={this.takePicture} color="danger">
+          <IonIcon icon={aperture} />
         </button>
         <IonAvatar className='avatarProfil'>
           <IonImg style={{ 'border': '1px solid black', 'minHeight': '100px' }} src={photo} ></IonImg>
