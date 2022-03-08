fa29345125e0d174d00b203a6669859907e97d51
pascalelavoieINF2050 <pascalelavoie75@gmail.com>
2022-03-07 23:32:09 -0500

    Comment tests of gestion du poids avec leur accord

--- src/App.test.js
+++ src/App.test.js
@@ -12,14 +12,13 @@ import DatePicker from "react-datepicker";
 const renderWithRouter = (ui, { route = '/' } = {}) => {
     window.history.pushState({}, 'Test page', route);
     return render(ui, { wrapper: BrowserRouter });
-    
 }
 
 test('renders without crashing', async() => {
     const { baseElement } = render(<App />);
     expect(baseElement).toBeDefined();
 });
-/*
+
 test('Check hydratation value', async() => {
     var attendu = 5;
     var something = { "hydratation": { "dailyTarget": { "value": 5, "unit": "", "globalConsumption": 0 }, "hydrates": ["0"] }, "alcool": { "dailyTarget": { "value": 0, "unit": "", "globalConsumption": 0 }, "alcools": [] }, "nourriture": { "globalConsumption": 0 }, "gras": { "dailyTarget": { "value": 0, "unit": "", "globalConsumption": 0 }, "grass": [] }, "proteines": { "dailyTarget": { "value": 0, "unit": "", "globalConsumption": 0 }, "proteines": [] }, "legumes": { "dailyTarget": { "value": 0, "unit": "", "globalConsumption": 0 }, "legumes": [] }, "cereales": { "dailyTarget": { "value": 0, "unit": "", "globalConsumption": 0 }, "cereales": [] }, "glycemie": { "dailyGlycemie": 0 }, "poids": { "dailyPoids": 0 }, "toilettes": { "feces": 0, "urine": 0 }, "sommeil": { "heure": 0, "minute": 0 }, "activities": { "heure": 0, "minute": 0 } };
