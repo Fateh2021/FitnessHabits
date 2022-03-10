c5d0ea78f835656ae22b31f52343946cef1219e9
Benoit <b.mignault@gmail.com>
2022-03-10 11:14:37 -0500

    Début de la branche pour corriger le bug 38

--- src/pages/Dashboard/Profil/Profil.jsx
+++ src/pages/Dashboard/Profil/Profil.jsx
@@ -1,25 +1,28 @@
 import React, { useState, Fragment } from "react";
 import Toggle from "./Toggle";
 import Sidebar from "./Sidebar";
+import Poids from '../ItemsList/Poids';
 
 const Profil = (props) => {
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-    const handlerOpen = () => {
-        setSidebarOpen(!sidebarOpen);
-    }
-    const sidebarCloseHandler = () => {
-        setSidebarOpen(false);
-        props.close();
-    }
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const handlerOpen = () => {
+    setSidebarOpen(!sidebarOpen);
+  }
+  const sidebarCloseHandler = () => {
+	// Préparation pour ajouter les informations qui vont mettre à jour IMC si la taille change
+	// Equipe Gestion - Poids avec autorisation de Équipe fittok
+    setSidebarOpen(false);
+    props.close();
+  }
 
-    let sidebar = sidebarOpen ? <Sidebar close={sidebarCloseHandler} sidebarClass="sidebar"/> : '';
-    
-    return (
-        <Fragment>
-            {sidebar}
-            <Toggle click={handlerOpen}/>
-            <p>{props.children}</p>
-        </Fragment>
-    )
+  let sidebar = sidebarOpen ? <Sidebar close={sidebarCloseHandler} sidebarClass="sidebar"/> : '';
+
+  return (
+    <Fragment>
+      {sidebar}
+      <Toggle click={handlerOpen}/>
+      <p>{props.children}</p>
+    </Fragment>
+  )
 }
 export default Profil;
