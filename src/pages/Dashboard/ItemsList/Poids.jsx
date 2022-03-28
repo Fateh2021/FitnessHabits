24e4d2da2b528893c9e735222053cbe311c6fb67
Benoit <b.mignault@gmail.com>
2022-03-28 12:05:18 -0400

    Finalisation des virgules + nettoyage des fichiers non nécessaire
    Sauf que j,ai en backup les fichiers dans mon dossier en parallèle au projet si nécessaire de faire un roll-back

--- src/pages/Dashboard/ItemsList/Poids.jsx
+++ src/pages/Dashboard/ItemsList/Poids.jsx
@@ -70,19 +70,19 @@ const Poids = (props) => {
         poidsService.setPrefUnitePoids(newUnitePoids);
         let oldUnitePoids = unitePoids;
     
-        setUnitePoids(newUnitePoids);
-        const dashboard = JSON.parse(localStorage.getItem("dashboard"));
-        var tmp_weight = 0;
+    setUnitePoids(newUnitePoids);
+    const dashboard = JSON.parse(localStorage.getItem("dashboard"));
+    var poidsAjuste = 0;
 
     if (oldUnitePoids === "KG" && newUnitePoids === "LBS") {
-      tmp_weight = (dashboard.poids.dailyPoids * 2.2).toFixed(1);
+      poidsAjuste = (dashboard.poids.dailyPoids * 2.2).toFixed(1);
     } else {
       // Nous sommes obliger de faire une modification de la valeur pour émettre une virgule apres le chiffre...
-      tmp_weight = (dashboard.poids.dailyPoids * 1.0).toFixed(1);
+      poidsAjuste = (dashboard.poids.dailyPoids * 1.0).toFixed(1);
     }
 
-        setDailyPoids(tmp_weight);
-        localStorage.setItem("dashboard", JSON.stringify(dashboard));
+    setDailyPoids(poidsAjuste);
+    localStorage.setItem("dashboard", JSON.stringify(dashboard));
 
     setImc(CalculImc());
   };
@@ -108,7 +108,8 @@ const Poids = (props) => {
         dashboard.poids.datePoids = new Date();
         localStorage.setItem("dashboard", JSON.stringify(dashboard));
 
-        setDailyPoids(poidsDaily);
+	  // Au moment du relaod de la page, nous allons avoir un seul chiffre après la virgule
+    setDailyPoids(poidsDaily);
 
         firebase
             .database()
