1350c84e1ccce566c12e74fd2eb315200ba52e14
Benoit <b.mignault@gmail.com>
2022-04-05 23:30:29 -0400

    Un oubli pour le dossier Poids

--- src/pages/Dashboard/test/Poids.test.js
+++ src/pages/Dashboard/test/Poids.test.js
@@ -6,7 +6,7 @@ import "@testing-library/jest-dom/extend-expect";
 import {act} from "react-dom/test-utils";
 import Poids from "../ItemsList/Poids";
 
-import { formatWeight, formatToKG , find_new_category, formatDate, initPrefWeight, setPrefUnitWeight} from '../../Poids/configuration/weightService';
+import { formatWeight, formatToKG , find_new_category, formatDate, initPrefWeight, setPrefUnitWeight} from '../../Weight/configuration/weightService';
 import { doneAll } from 'ionicons/icons';
 
 jest.mock("firebase", () => {
