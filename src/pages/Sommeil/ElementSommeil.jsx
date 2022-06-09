ec2e2eb25f46459df778c5c0b6630ac29b62cd27
AlexandreLachanceGit <alexandrelachance@me.com>
2022-06-09 19:58:59 -0400

    Ameliorer style de la liste de sommeils.

--- src/pages/Sommeil/ElementSommeil.jsx
+++ src/pages/Sommeil/ElementSommeil.jsx
@@ -1,16 +1,28 @@
 import { IonItem, IonText } from "@ionic/react";
 import React from "react";
+import "./Sommeil.css";
 
 function ElementSommeil(props) {
   return (
-    <IonItem>
-      <IonText>
-        De {props.info.start} a {props.info.end} - reveille en {props.info.mood}{" "}
+    <ion-row class="modal-data">
+      <ion-col>
+        <IonText color="dark" class="data-time">
+          De <b>{props.info.start}</b> à{" "}
+          <b>
+            {props.info.end} - réveillé {props.info.mood}
+          </b>
+        </IonText>
         <br />
-        Durant cette periode, je me suis reveille {props.info.nbAwoken} <br />
-        {props.info.day} / {props.info.month} / {props.info.year}
-      </IonText>
-    </IonItem>
+        <IonText color="dark" class="data-text">
+          Durant cette période, je me suis réveillé {props.info.nbAwoken} fois.
+        </IonText>
+        <br />
+        <IonText color="dark" class="data-date">
+          {props.info.day} / {props.info.month} / {props.info.year}
+        </IonText>
+        <div class="hr"></div>
+      </ion-col>
+    </ion-row>
   );
 }
 
