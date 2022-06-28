a8529f7bbc586b953ce337c1f075a5717661d07b
Julien Champagne <jchampanzee@gmail.com>
2022-06-28 16:35:27 -0400

    Ajout des éléments d'affichage pour le nombre de fois réveillé dans l'ajout d'une période de sommeil

--- src/pages/Sommeil/AjoutPeriodeForm.jsx
+++ src/pages/Sommeil/AjoutPeriodeForm.jsx
@@ -18,7 +18,17 @@ function AjoutPeriodeForm() {
     });
   };
 
-  return <form></form>;
+  return (
+    <form>
+  <ion-grid>      
+  <ion-row> <ion-item class="ajout-item">
+	<ion-col><ion-label class="ajout-label-nbrFois">Durant cette période,<br/> je me suis réveillé</ion-label></ion-col>
+	<ion-col><ion-input id="nbAwoken" type="number"></ion-input></ion-col>
+	<ion-col><ion-label>fois</ion-label></ion-col>
+  </ion-item></ion-row>
+  </ion-grid>
+    </form>
+);
 }
 
 export default AjoutPeriodeForm;
